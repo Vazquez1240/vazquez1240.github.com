5a25ad3fd16ede344c0e9ca4ac9a26d93d1be6f3
Martin de la torre vazquez <127373642+Vazquez1240@users.noreply.github.com>
2023-04-17 23:10:28 -0600

    ultimo commit

--- voletines/javascript/main.js
+++ voletines/javascript/main.js
@@ -3,7 +3,23 @@ let links = document.querySelectorAll(".close");
 
 links.forEach(function(link){
 
-    link.addEventListener("click",function(){
+    link.addEventListener("click",function(ev){
+        ev.preventDefault();
+        let content = document.querySelector(".content");
+
+        content.classList.remove("fadeInDown");
+        content.classList.remove("animated");
+
+        
+        content.classList.remove("fadeOuUp");
+        content.classList.add("animated");
+
+        setTimeout(function(){
+            location.href = "/";
+        },1000);
+        
+        
+        
         
     })
 })
