51cb2fc22112d7aba007e9fd90990ca0b700d588
Martin de la torre vazquez <127373642+Vazquez1240@users.noreply.github.com>
2023-06-02 00:03:26 -0600

    carpta de imagenes

--- clima/index.js
+++ clima/index.js
@@ -36,23 +36,23 @@ search.addEventListener('click', () => {
 
             switch (json.weather[0].main) {
                 case 'Clear':
-                    image.src = 'images/clear.png';
+                    image.src = 'img/clear.png';
                     break;
 
                 case 'Rain':
-                    image.src = 'images/rain.png';
+                    image.src = 'img/rain.png';
                     break;
 
                 case 'Snow':
-                    image.src = 'images/snow.png';
+                    image.src = 'img/snow.png';
                     break;
 
                 case 'Clouds':
-                    image.src = 'images/cloud.png';
+                    image.src = 'img/cloud.png';
                     break;
 
                 case 'Haze':
-                    image.src = 'images/mist.png';
+                    image.src = 'img/mist.png';
                     break;
 
                 default:
