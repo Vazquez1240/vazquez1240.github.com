bbe5634ddd9d85327d1981583ba62c5a489db058
vazquez1240 <vazquezmartin1240@gmail.com>
2023-07-03 20:48:30 -0600

    se intento arreglar la pokeapi

---
+++ pokeapi/app.js
@@ -0,0 +1,55 @@
+let url = 'https://pokeapi.co/api/v2/pokemon/';
+const pokemonData = [];
+for (let i = 1; i <= 151; i++){
+    fetch(`https://pokeapi.co/api/v2/pokemon/${i}`)
+    .then((response) => response.json())
+    .then( data => {
+        pokemonData.push(data); // Almacenar los datos en el arreglo pokemonData
+  
+        // Verificar si se han obtenido todos los datos
+        if (pokemonData.length === 151) {
+          // Ordenar los datos por número de Pokémon
+          pokemonData.sort((a, b) => a.id - b.id);
+  
+          // Mostrar los datos
+          pokemonData.forEach((pokemon) => mostrarData(pokemon));
+        }
+      }).catch((error) => {
+        console.log(error);
+      })
+}
+
+
+function mostrarData(data){
+
+    const linea = document.createElement('div');
+    linea.classList.add('card');
+
+    let tipos = data.types.map((type) => 
+        `<p class="${type.type.name} tipo-pokemon">${type.type.name}</p>`)
+    tipos = tipos.join('');
+
+    let pokeId = data.id.toString();
+    if (pokeId.length === 1) {
+        pokeId = "00" + pokeId;
+    } else if (pokeId.length === 2) {
+        pokeId = "0" + pokeId;
+    }
+
+    const contenido = `
+    <div class="card-content">
+        <img src=${data.sprites.other['official-artwork'].front_default} alt=${data.name}>
+        <h2>${data.name}</h2>
+        <p class="numero-pokemon">#${pokeId}</p>
+        <div class='poke-tipos'>
+            ${tipos}
+        </div>
+        <div class="poke-datos">
+            <p>${data.height}m</p>
+            <p>${data.weight}Kg</p>
+        </div>
+    </div>`
+    linea.innerHTML = contenido;
+
+    contenidos.append(linea)
+}
